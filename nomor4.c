1b075386a0e6ecb6bbd825724bb1e26444f69875
cornelianat <cornelianatasha528@gmail.com>
2017-10-25 12:19:46 +0700

    haii

--- nomor4.c
+++ nomor4.c
@@ -14,45 +14,41 @@ void *prime(void* end)
 {
     printf("\n");
 
-    pthread_mutex_lock(&lock);
-
+    //pthread_mutex_lock(&lock);
+    //printf("%s\n",(char*)end);
+  
     int jawaban=0;
-    int *a = (int *) end;
-    int jawab=*a;
+    int a = (int) end;
+    int jawab=(int)strtol((char *)end,NULL,10);
+    int temp=jawab;
+    //printf("%d\n",jawab);
     int isPrime;
         for(int i=jawab-1; i>=1; i--)
         {
             jawab=jawab*i;
-            //printf("%d",jawab);
         }
-        printf("%d",jawab);
+        printf("%d = %d\n",temp,jawab);
 
     //printf ("awal is %d, simpan is %d", awal, simpan);
-    pthread_mutex_unlock(&lock);
+    //pthread_mutex_unlock(&lock);
 
     return NULL;
 }
 
-int main (){
+int main (int argc, char *argv[]){
     pthread_t tid[1000];
     int t, no;
-   // scanf("%c",&fakt);
-    printf ("how many threads?\n");
-    scanf ("%d",&t);
 
-    for(int i=0;i<t;i++)
+    for(int i=0;i<argc-1;i++)
     {
-        scanf("%d",&simpan);
-        pthread_create(&(tid[i]),NULL,prime,&simpan);
+        //scanf("%d",&simpan);
+	//printf("%s",argv[i+1]);
+        pthread_create(&(tid[i]),NULL,&prime,(void *)argv[i+1]);
     }
 
     //s=simpan;
-    for (int i=0;i<t;i++)
-    {
-
-    }
-
-    for (int i=0;i<t;i++)
+   
+    for (int i=0;i<argc-1;i++)
     {
         pthread_join((tid[i]),NULL);
     }
